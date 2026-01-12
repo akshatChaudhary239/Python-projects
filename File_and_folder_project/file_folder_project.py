ebf4f9f95ee71d5e6b9e4f81d08d6d22d7803d09
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2026-01-13 02:37:44 +0530

    "Done"

--- File_and_folder_project/file_folder_project.py
+++ File_and_folder_project/file_folder_project.py
@@ -1,1 +1,10 @@
-# File and Folder project
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hello my name is akku and iam doing a great job curently")
+engine.runAndWait()
+
+
+def parse_command(command):
+    command = command.lower()
+    if "create" in command:
+        words = command
